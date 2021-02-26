848fea012d16ee2235a8dd30cefcee9f403b4261
Jakub LevÃÂ½ <jakub.levy15@gmail.com>
2021-02-26 21:44:18 +0100

    corrections

--- kindle-comic-converter/tools/chocolateyinstall.ps1
+++ kindle-comic-converter/tools/chocolateyinstall.ps1
@@ -1,24 +1,15 @@
 ﻿$ErrorActionPreference = 'Stop';
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = ''
 $url64      = 'https://kcc.iosphe.re/Windows/'
 
 $packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
-  unzipLocation = $toolsDir
-  fileType      = 'EXE'
-  url           = $url
-  url64bit      = $url64
-
-  softwareName  = 'Kindle Comic Converter*'
-
-  checksum      = ''
-  checksumType  = 'sha256'
-  checksum64    = 'E465225BEDC50523397F9094914722897DC56509C7FCB6861AC815A94D638DDE'
-  checksumType64= 'sha256'
-
-  silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
-  validExitCodes= @(0)
+  packageName    = $env:ChocolateyPackageName
+  fileType       = 'EXE'
+  url64bit       = $url64
+  softwareName   = 'Kindle Comic Converter*'
+  checksum64     = 'E465225BEDC50523397F9094914722897DC56509C7FCB6861AC815A94D638DDE'
+  checksumType64 = 'sha256'
+  silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  validExitCodes = @(0)
 }
 
 $addionalArgs = Get-PackageParameters
