a4d70b80912c57b1296d207ef986103ce1c521cb
Jakub LevÃÂ½ <jakub.levy15@gmail.com>
2021-03-19 20:31:07 +0100

    winfetch install command changed

--- winfetch/tools/chocolateyinstall.ps1
+++ winfetch/tools/chocolateyinstall.ps1
@@ -3,6 +3,7 @@ $toolsDir              = "$(Split-Path -parent $MyInvocation.MyCommand.Definitio
 $version               = '2.0.0'
 $executableFullPath    = "$toolsDir\winfetch-$version\winfetch.ps1"
 $url                   = "https://github.com/lptstr/winfetch/archive/refs/tags/v$version.zip"
+$checksum              = '53EDA3231F7A556076E65B098FF48619739BA0FBEA518E4998B3615A318A218A'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -10,14 +11,13 @@ $packageArgs = @{
   url           = $url
   url64         = $url
   softwareName  = 'winfetch*'
-  checksum      = '53EDA3231F7A556076E65B098FF48619739BA0FBEA518E4998B3615A318A218A'
+  checksum      = $checksum
   checksum64    = $checksum
   checksumType  = 'sha256' 
 }
 
 Install-ChocolateyZipPackage @packageArgs
 
-Install-BinFile `
-  -Name 'winfetch' `
-  -Command """$executableFullPath"" %*" `
-  -Path "$env:SystemRoot\System32\WindowsPowerShell\v1.0\powershell.exe"
+Install-ChocolateyPowershellCommand `
+  -PackageName    "$packageArgs.$packageName.powershell" `
+  -PsFileFullPath $executableFullPath
