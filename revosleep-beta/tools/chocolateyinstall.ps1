ae89600e919041af326f7618f520505cb90ff5c5
Jakub LevÃÂ½ <jakub.levy15@gmail.com>
2021-03-20 21:05:55 +0100

    kindle-comic-converter update.ps1 added

--- revosleep-beta/tools/chocolateyinstall.ps1
+++ revosleep-beta/tools/chocolateyinstall.ps1
@@ -5,7 +5,7 @@ $checksum              = '010B45AFB2FCB0CD5ECD1241708F6E5792DFECA6A21160A243EE45
 $checksum64            = 'E79A61803992083808ECC653896581A285A88D18EC63556E0122B83AC3142D94'
 
 # Required – download links contain session id
-$page          = Invoke-WebRequest -UseBasicParsing 'https://www.meinfach.net/revosleep/'
+$page          = Invoke-WebRequest -UseBasicParsing -Uri 'https://www.meinfach.net/revosleep/'
 $downloadLinks = $page.Links     | Where-Object href -match $version 
 $url           = ($downloadLinks | Where-Object class -match "revoSleepx86" | Select-Object -First 1 -exp href) -Replace ';', '&'
 $url64         = ($downloadLinks | Where-Object class -match "revoSleepx64" | Select-Object -First 1 -exp href) -Replace ';', '&'
