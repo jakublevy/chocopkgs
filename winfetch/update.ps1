a72713c692e71e769430df2fbae68903cc30c4d2
Jakub LevÃÂ½ <jakub.levy15@gmail.com>
2021-03-20 21:48:19 +0100

    revosleep update.ps1 added

--- winfetch/update.ps1
+++ winfetch/update.ps1
@@ -2,7 +2,7 @@ import-module au
 
 function global:au_SearchReplace {
     @{
-        ".\tools\chocolateyInstall.ps1" = @{
+        ".\tools\chocolateyInstall.ps1"   = @{
             "(^[$]version\s*=\s*)('.*')"  = "`$1'$($Latest.Version)'"
             "(^[$]checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum)'"
         }
@@ -11,7 +11,7 @@ function global:au_SearchReplace {
 
 function global:au_GetLatest {
     $download_page = Invoke-WebRequest -UseBasicParsing -Uri 'https://github.com/lptstr/winfetch/releases/'
-    $relative_url = $download_page.links | Where-Object href -match '/lptstr/winfetch/archive/refs/tags/v\d+\.\d+\.\d+\.zip' | Select-Object -First 1 -expand href
+    $relative_url  = $download_page.links | Where-Object href -match '/lptstr/winfetch/archive/refs/tags/v\d+\.\d+\.\d+\.zip' | Select-Object -First 1 -expand href
     $version = ([regex]::Match($relative_url, '(\d+\.\d+\.\d+)\.zip')).Groups[1].Value
 
     Invoke-WebRequest -UseBasicParsing "https://github.com$relative_url" -OutFile '_winfetch.zip'
