5ea95a2312f5c0a6df247d4c2a170ef87d053630
Jakub Levý <jakub.levy15@gmail.com>
2021-03-22 00:25:53 +0100

    revosleep shim added

---
+++ revosleep/tools/chocolateyuninstall.ps1
@@ -0,0 +1,28 @@
+﻿$ErrorActionPreference = 'Stop';
+$executableName = 'revoSleep'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  softwareName  = "$env:ChocolateyPackageName*"
+  fileType      = 'MSI'
+  silentArgs    = "/qn /norestart"
+  validExitCodes= @(0, 3010, 1605, 1614, 1641)
+}
+
+[array]$keys = Get-UninstallRegistryKey -SoftwareName $packageArgs['softwareName']
+if ($keys.Count -eq 1) {
+  $installLocation = Get-AppInstallLocation $packageArgs['softwareName']
+  Uninstall-BinFile `
+    -Name $executableName `
+    -Path "$installLocation\$executableName.exe"
+
+  $packageArgs['silentArgs'] = "$($keys[0].PSChildName) $($packageArgs['silentArgs'])"
+  Uninstall-ChocolateyPackage @packageArgs
+} elseif ($keys.Count -eq 0) {
+  Write-Warning "$packageName has already been uninstalled by other means."
+} elseif ($keys.Count -gt 1) {
+  Write-Warning "$($keys.Count) matches found!"
+  Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
+  Write-Warning "Please alert package maintainer the following keys were matched:"
+  $keys | % {Write-Warning "- $($_.DisplayName)"}
+}
