0635602f880ddfa8fbe21795cb41842cfb109f8e
Jakub LevÃÂ½ <jakub.levy15@gmail.com>
2021-03-24 09:56:37 +0100

    update.ps1 updates release notes

--- deadbeef/update.ps1
+++ deadbeef/update.ps1
@@ -1,10 +1,15 @@
 import-module au
 
 function global:au_SearchReplace {
+    $url64 = "https://sourceforge.net/projects/deadbeef/files/travis/windows/$version/deadbeef-$version-windows-x86_64.exe"
+    Invoke-WebRequest -Uri $url64 -OutFile '_deadbeef.exe'
+    $checksum64 = (Get-FileHash '_deadbeef.exe' -Algorithm SHA256).Hash
+    Remove-Item '_deadbeef.exe' -Force
+
     @{
-        ".\tools\chocolateyInstall.ps1"   = @{
+        ".\tools\chocolateyinstall.ps1"   = @{
             "(^[$]version\s*=\s*)('.*')"  = "`$1'$($Latest.Version)'"
-            "(^[$]checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+            "(^[$]checksum64\s*=\s*)('.*')" = "`$1'$checksum64'"
         }
         ".\deadbeef.nuspec"   = @{
             "(?i)(\<releaseNotes\>).*(\<\/releaseNotes\>)" = "`${1}$($Latest.ReleaseNotes)`${2}"
@@ -17,14 +22,8 @@ function global:au_GetLatest {
     $relative_urls  = $download_page.links | ? href -match ".*/\d+\.\d+(\.\d+)*/$" | select -exp href
     $versions = $relative_urls | % { ([regex]::Match($_, '.*/(\d+\.\d+(\.\d+)*)/$')).Groups[1].Value }
     $version = $versions | sort -Descending {[version] $_ } | select -First 1
-    $url64 = "https://sourceforge.net/projects/deadbeef/files/travis/windows/$version/deadbeef-$version-windows-x86_64.exe"
-    Invoke-WebRequest -Uri $url64 -OutFile '_deadbeef.exe'
-    $checksum64 = (Get-FileHash '_deadbeef.exe' -Algorithm SHA256).Hash
-    Remove-Item '_deadbeef.exe' -Force
-
     @{
         Version  = $version
-        Checksum64 = $checksum64
         ReleaseNotes = "https://github.com/DeaDBeeF-Player/deadbeef/blob/$version/ChangeLog"
     }
 }
--- winfetch/update.ps1
+++ winfetch/update.ps1
@@ -1,10 +1,14 @@
 import-module au
 
 function global:au_SearchReplace {
+    Invoke-WebRequest -UseBasicParsing "https://github.com$($Latest.RelativeUrl)" -OutFile '_winfetch.zip'
+    $checksum = (Get-FileHash '_winfetch.zip' -Algorithm SHA256).Hash
+    Remove-Item '_winfetch.zip' -Force
+
     @{
-        ".\tools\chocolateyInstall.ps1"   = @{
+        ".\tools\chocolateyinstall.ps1"   = @{
             "(^[$]version\s*=\s*)('.*')"  = "`$1'$($Latest.Version)'"
-            "(^[$]checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum)'"
+            "(^[$]checksum\s*=\s*)('.*')" = "`$1'$checksum'"
         }
         ".\winfetch.nuspec"   = @{
             "(?i)(\<releaseNotes\>).*(\<\/releaseNotes\>)" = "`${1}$($Latest.ReleaseNotes)`${2}"
@@ -16,15 +20,10 @@ function global:au_GetLatest {
     $download_page = Invoke-WebRequest -UseBasicParsing -Uri 'https://github.com/lptstr/winfetch/releases/'
     $relative_url  = $download_page.links | Where-Object href -match '/lptstr/winfetch/archive/refs/tags/v\d+\.\d+(\.\d+)*\.zip' | Select-Object -First 1 -expand href
     $version = ([regex]::Match($relative_url, '(\d+\.\d+(\.\d+)*)\.zip')).Groups[1].Value
-
-    Invoke-WebRequest -UseBasicParsing "https://github.com$relative_url" -OutFile '_winfetch.zip'
-    $checksum = (Get-FileHash '_winfetch.zip' -Algorithm SHA256).Hash
-    Remove-Item '_winfetch.zip' -Force
-
     @{
         Version      = $version
-        Checksum     = $checksum
         ReleaseNotes = "https://github.com/lptstr/winfetch/releases/tag/v$version"
+        RelativeUrl  = $relative_url
     }
 }
 
