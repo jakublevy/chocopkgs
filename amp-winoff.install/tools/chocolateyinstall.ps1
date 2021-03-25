7206456786c862496a4bbce76ba46f44191869f0
Jakub Levý <jakub.levy15@gmail.com>
2021-03-25 18:53:00 +0100

    amp-winoff added

---
+++ amp-winoff.install/tools/chocolateyinstall.ps1
@@ -0,0 +1,20 @@
+﻿$ErrorActionPreference = 'Stop';
+$checksum = '2891D5CD484791A8482865255333B8CF432A9D6DFB8705D724A825B85B64C891'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  fileType      = 'EXE'
+  url           = 'https://www.ampsoft.net/files/WinOFFSetup.exe'
+  softwareName  = 'AMP WinOFF*'
+  checksum      = $checksum
+  checksumType  = 'sha256'
+  silentArgs    = "/S"
+  validExitCodes= @(0)
+}
+
+$additionalArgs = Get-PackageParameters
+if($additionalArgs['InstallationPath']) {
+  $packageArgs['silentArgs'] += " /D=$($additionalArgs['InstallationPath'])"
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ amp-winoff.portable/tools/chocolateyinstall.ps1
@@ -0,0 +1,37 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir = $(Split-Path -Parent $MyInvocation.MyCommand.Definition)
+$checksum = 'AF0637EBC5122A93C4DA49EFB2E29CAF999EBBE0C4352A7628732F612893F43D'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  url           = 'https://www.ampsoft.net/files/WinOFF.zip'
+  checksum      = $checksum
+  checksumType  = 'sha256'
+  unzipLocation = "$toolsDir\bin"
+  silentArgs    = "/S"
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyZipPackage @packageArgs
+
+$shimArgs = @{
+  path       = $packageArgs['unzipLocation']
+  itemType   = 'file'
+  errorAction= 'SilentlyContinue'
+}
+
+New-Item `
+  -Name 'WinOFF.exe.gui' `
+  @shimArgs
+
+New-Item `
+  -Name 'WinOFF_admin.exe.ignore' `
+  @shimArgs
+
+New-Item `
+  -Name 'WinOFF_guardian.exe.ignore' `
+  @shimArgs
+
+New-Item `
+  -Name 'WinOFF_launcher.exe.ignore' `
+  @shimArgs
