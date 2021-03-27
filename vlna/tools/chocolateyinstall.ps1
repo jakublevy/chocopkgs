edcafddc26f1cf227d8ffdaf5bd44253a1bbe343
Jakub Levý <jakub.levy15@gmail.com>
2021-03-27 18:11:43 +0100

    vlna added

---
+++ vlna/tools/chocolateyinstall.ps1
@@ -0,0 +1,13 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir = $(Split-Path -parent $MyInvocation.MyCommand.Definition)         
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  softwareName  = "$env:ChocolateyPackageName*"
+  url           = "https://petr.olsak.net/ftp/olsak/vlna/oldbin/vlna32.exe"
+  checksum      = '0ECB6B4284210AE430F7ECB4DF5713914D6455213118B8BE9302B243FEFBCADA'
+  checksumType  = 'sha256' 
+  fileFullPath  = "$toolsDir\vlna.exe"
+}
+
+Get-ChocolateyWebFile @packageArgs
