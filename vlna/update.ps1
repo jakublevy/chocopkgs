37d31719d62d75e1d9f2cab81daa5afb1ec676ef
Jakub LevÃÂ½ <jakub.levy15@gmail.com>
2021-03-29 09:40:43 +0200

    update.ps1 for vlna improved

--- vlna/update.ps1
+++ vlna/update.ps1
@@ -1,16 +1,33 @@
 import-module au
 
 # manual
+function global:au_SearchReplace {
+    @{
+    }
+}
 
 function global:au_GetLatest {
-    $download_page = Invoke-WebRequest -UseBasicParsing -Uri 'http://petr.olsak.net/ftp/olsak'
-    $no_new_version_available = $download_page.Content -match '2010-02-01 23:04'
-    if($no_new_version_available) {
-        $version = '1.2'
+    $version = '1.2'
+    $base_url = 'http://petr.olsak.net/ftp/olsak/'
+    $regex = [regex]::new('\d{4}-\d{2}-\d{2}')
+    $relative_urls = [System.Collections.Queue]::new()
+    $relative_urls.Enqueue('vlna/')
+    
+    while($relative_urls.Count -gt 0) {
+        $curr_relative_url = $relative_urls.Dequeue()
+        $url = "$base_url$curr_relative_url"
+        $response = Invoke-WebRequest -UseBasicParsing -Uri $url
+
+        $sub_dirs = $response.links | % href | ? { $_.EndsWith('/') -And -Not $_.StartsWith('/') }
+        $sub_dirs | % { $relative_urls.Enqueue("$curr_relative_url$_") }
+
+        $newest_change = $regex.Matches($response.Content) | % { [datetime]::Parse($_) } | sort -Descending | select -First 1
+        if($newest_change.Year -gt 2010) {
+            $version = '1.6'
+            break
+        }
     }
-    else {
-        $version = '1.6'
-    }
+
     @{
         Version  = $version
     }
