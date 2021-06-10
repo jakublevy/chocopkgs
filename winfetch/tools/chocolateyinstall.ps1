aca35297c9b775a2f5a6cb0e007a867d08aa0142
Jakub Levý <jakub.levy15@gmail.com>
2021-06-10 13:57:15 +0200

    netlimiter updated to 4.1.10.0

--- winfetch/tools/chocolateyinstall.ps1
+++ winfetch/tools/chocolateyinstall.ps1
@@ -5,7 +5,7 @@ $checksum = 'EF7CADAB4CEC7E1BFE6CFABEF3AF36E3F0F8ABC1F53DCF5E3B6E06AB4FE94179'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
-  url           = "https://raw.githubusercontent.com/lptstr/winfetch/v$version/winfetch.ps1"
+  url           = "https://raw.githubusercontent.com/kiedtl/winfetch/v$version/winfetch.ps1"
   checksum      = $checksum
   checksumType  = 'sha256' 
   psFileFullPath= "$toolsDir\winfetch.ps1"
