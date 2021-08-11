2a355f18ce818bb97ee586899b493bd25d0618a7
Jakub LevÃÂ½ <jakub.levy15@gmail.com>
2021-08-11 16:27:58 +0200

    proficad pkg added

---
+++ proficad/tools/chocolateybeforemodify.ps1
@@ -0,0 +1,4 @@
+﻿$kcc = Get-Process -Name "proficad" -ErrorAction SilentlyContinue
+if($kcc) {
+    Stop-Process $kcc -Force
+}
---
+++ proficad/tools/chocolateyinstall.ps1
@@ -0,0 +1,23 @@
+﻿$ErrorActionPreference = 'Stop';
+$checksum = 'D82E5273C7A7AC85F9CF46DEF6EDC458C5A2CBE8D129AB202969D15219E639AB'
+$version  = '11.2.2'
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  fileType       = 'EXE'
+  url            = "https://www.proficad.com/down/$($version.Split('.')[0])/setup_full.exe"
+  softwareName   = 'Kindle Comic Converter*'
+  checksum       = $checksum
+  checksumType   = 'sha256'
+  silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  validExitCodes = @(0)
+}
+
+$addionalArgs = Get-PackageParameters
+
+if($addionalArgs['InstallationPath']) {
+  $path = $addionalArgs['InstallationPath']
+  $packageArgs['silentArgs'] += " /DIR=""$path"""
+}
+
+Install-ChocolateyPackage @packageArgs
