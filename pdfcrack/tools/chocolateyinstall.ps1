a740aa890e8cafa4c5cc61cbc55eb468bb711d19
Jakub LevÃÂ½ <jakub.levy15@gmail.com>
2021-08-17 10:33:22 +0200

    pdfcrack added

---
+++ pdfcrack/tools/chocolateyinstall.ps1
@@ -0,0 +1,11 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir = $(Split-Path -Parent $MyInvocation.MyCommand.Definition)
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  fileFullPath64= Join-Path $toolsDir 'pdfcrack-v0.19-windows-amd64.zip'
+  destination   = "$toolsDir\bin"
+  validExitCodes= @(0)
+}
+
+Get-ChocolateyUnzip @packageArgs
