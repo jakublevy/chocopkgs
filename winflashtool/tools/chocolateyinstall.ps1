753c373cc99a08346159243077e53049d0e15992
Jakub Levý <jakub.levy15@gmail.com>
2021-08-22 21:46:20 +0200

    winflashtool added

---
+++ winflashtool/tools/chocolateyinstall.ps1
@@ -0,0 +1,8 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+$version = '3.0'
+
+Rename-Item `
+  -Path "$toolsDir\WinFLASHTool-$version.exe" `
+  -NewName "WinFLASHTool.exe" `
+  -Force
