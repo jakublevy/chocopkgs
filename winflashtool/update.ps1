7b06255d13bfb1a808742fc93aa3efb50c682bc8
Jakub LevÃÂ½ <jakub.levy15@gmail.com>
2021-08-22 21:46:20 +0200

    winflashtool added

---
+++ winflashtool/update.ps1
@@ -0,0 +1,29 @@
+import-module au
+
+function global:au_SearchReplace {
+    @{
+        ".\tools\chocolateyinstall.ps1"   = @{
+            "(^[$]version\s*=\s*)('.*')"  = "`$1'$($Latest.Version)'"
+        }
+        ".\legal\VERIFICATION.txt" = @{
+            "(?i)(\s+Go to).*"           = "`${1} $($Latest.Url64)"
+            "(?i)(\s+checksum64:).*"     = "`${1} $($Latest.Checksum64)"
+        }
+    }
+}
+
+function global:au_GetLatest {
+    $download_page = Invoke-WebRequest -UseBasicParsing -Uri 'https://sysprogs.com/winflashtool/download/'
+    $relative_url  = $download_page.Links | Where-Object href -match 'WinFLASHTool-' | Select-Object -First 1 -expand href
+    $version     = ([regex]::Match($relative_url, '/WinFLASHTool-(\d+\.\d+(\.\d+)*)\.exe')).Groups[1].Value
+    @{
+        Version      = $version
+        Url64        = "https://sysprogs.com/getfile/1087/WinFLASHTool-$version.exe"
+    }
+}
+
+function global:au_BeforeUpdate {
+    Get-RemoteFiles -NoSuffix -Purge
+}
+
+Update-Package -ChecksumFor None
