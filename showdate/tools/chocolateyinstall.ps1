e2b00f0612b46567b3f6cf5a58b8f84b46bcbfdb
Jakub Levý <jakub.levy15@gmail.com>
2021-08-22 10:12:52 +0200

    showdate, adreports added

---
+++ showdate/tools/chocolateyinstall.ps1
@@ -0,0 +1,22 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  fileFullPath   = Join-Path $toolsDir 'showdate-0.1.1-win32.zip'
+  fileFullPath64 = Join-Path $toolsDir 'showdate-0.1.1-win64.zip'
+  destination    = $toolsDir
+  validExitCodes = @(0)
+}
+
+Get-ChocolateyUnzip @packageArgs
+
+Remove-Item `
+  -Path $packageArgs['fileFullPath'] `
+  -ErrorAction SilentlyContinue `
+  -Force
+
+Remove-Item `
+  -Path $packageArgs['fileFullPath64'] `
+  -ErrorAction SilentlyContinue `
+  -Force
