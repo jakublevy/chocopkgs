47f619a267cf08f4d9f16fc94797f4166dc85b1c
Jakub LevÃÂ½ <jakub.levy15@gmail.com>
2021-08-23 13:55:01 +0200

    analyzer2go added

---
+++ analyzer2go/update.ps1
@@ -0,0 +1,22 @@
+import-module au
+
+function global:au_SearchReplace {
+    @{
+        ".\tools\chocolateyinstall.ps1"   = @{
+            "(^[$]absUrl\s*=\s*)('.*')"  = "`$1'$($Latest.Url32)'"
+        }
+    }
+}
+
+function global:au_GetLatest {
+    $download_page = Invoke-WebRequest -UseBasicParsing -Uri 'https://sysprogs.com/analyzer2go/download'
+    $absoluteUrl   = $download_page.Links | Where-Object href -match '-trial\.msi' | Select-Object -First 1 -expand href
+    $version       = ([regex]::Match($absoluteUrl, '/Analyzer2Go-(\d+\.\d+(\.\d+)*(r\d+)?)-trial\.msi')).Groups[1].Value
+    $versionChoco  = $version.Replace('r', '.')
+    @{
+        Version      = $versionChoco
+        Url32        = $absoluteUrl
+    }
+}
+
+Update-Package
