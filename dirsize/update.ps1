ff997ea37688568afa16cad100c6b151d7a30350
Jakub LevÃÂ½ <jakub.levy15@gmail.com>
2021-08-24 15:04:24 +0200

    dirsize added

---
+++ dirsize/update.ps1
@@ -0,0 +1,37 @@
+import-module au
+
+function global:au_SearchReplace {
+    @{
+        ".\tools\chocolateyinstall.ps1"   = @{
+            "(?i)(^\s*fileFullPath\s*=\s*)(.*)"    = "`${1}Join-Path `$toolsDir '$($Latest.FileName32)'"
+            "(?i)(^\s*fileFullPath64\s*=\s*)(.*)"  = "`${1}Join-Path `$toolsDir '$($Latest.FileName64)'"
+        }
+        ".\legal\VERIFICATION.txt" = @{
+            "(?i)(\s+x86:).*"          = "`${1} $($Latest.Url32)"
+            "(?i)(\s+x64:).*"          = "`${1} $($Latest.Url64)"
+            "(?i)(\s+checksum32:).*"   = "`${1} $($Latest.Checksum32)"
+            "(?i)(\s+checksum64:).*"   = "`${1} $($Latest.Checksum64)"
+        }
+        ".\dirsize.nuspec" = @{
+            "(?i)(\<releaseNotes\>).*(\<\/releaseNotes\>)" = "`${1}$($Latest.ReleaseNotes)`${2}"
+        }
+    }
+}
+
+function global:au_GetLatest {
+    $download_page = Invoke-WebRequest -UseBasicParsing -Uri 'https://github.com/shenwei356/dirsize/releases'
+    $relative_url  = $download_page.links | Where-Object href -match '/shenwei356/dirsize/releases/download/v\d+\.\d+(\.\d+)*/dirsize_windows_amd64\.exe\.tar\.gz' | Select-Object -First 1 -expand href
+    $version = ([regex]::Match($relative_url, 'v(\d+\.\d+(\.\d+)*)')).Groups[1].Value
+    @{
+        Version      = $version
+        Url32        = "https://github.com/shenwei356/dirsize/releases/download/v$version/dirsize_windows_386.exe.tar.gz"
+        Url64        = "https://github.com/shenwei356/dirsize/releases/download/v$version/dirsize_windows_amd64.exe.tar.gz"
+        ReleaseNotes = "https://github.com/shenwei356/dirsize/releases/tag/v$version"
+    }
+}
+
+function global:au_BeforeUpdate {
+    Get-RemoteFiles -NoSuffix -Purge
+}
+
+Update-Package -ChecksumFor None
