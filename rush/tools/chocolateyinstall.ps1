cb654e3610deeacb7ba373099ab7cb8cca5d52a9
Jakub Levý <jakub.levy15@gmail.com>
2021-08-24 12:31:30 +0200

    rush added

---
+++ rush/tools/chocolateyinstall.ps1
@@ -0,0 +1,27 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  fileFullPath   = Join-Path $toolsDir 'rush_windows_386.exe.tar.gz'
+  fileFullPath64 = Join-Path $toolsDir 'rush_windows_amd64.exe.tar.gz'
+  destination    = $toolsDir
+  validExitCodes = @(0)
+}
+
+Get-ChocolateyUnzip @packageArgs
+
+$packageArgs['fileFullPath']   = $packageArgs['fileFullPath'].Replace('.gz', '')
+$packageArgs['fileFullPath64'] = $packageArgs['fileFullPath64'].Replace('.gz', '')
+$packageArgs['destination']    = "$toolsDir\bin"
+Get-ChocolateyUnzip @packageArgs
+
+Remove-Item `
+  -Path "$toolsDir\*.tar" `
+  -ErrorAction SilentlyContinue `
+  -Force
+
+Remove-Item `
+  -Path "$toolsDir\*.gz" `
+  -ErrorAction SilentlyContinue `
+  -Force
