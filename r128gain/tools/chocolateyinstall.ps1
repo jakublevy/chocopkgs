ac535d2f6d65b2598541942298c528caface0d38
Jakub LevÃÂ½ <jakub.levy15@gmail.com>
2021-08-30 13:00:41 +0200

    r128gain added

---
+++ r128gain/tools/chocolateyinstall.ps1
@@ -0,0 +1,27 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  fileFullPath   = Join-Path $toolsDir 'r128gain_win32.7z'
+  fileFullPath64 = Join-Path $toolsDir 'r128gain_win64.7z'
+  destination    = $toolsDir
+  validExitCodes = @(0)
+}
+
+Get-ChocolateyUnzip @packageArgs
+
+$filesToRemove = @($packageArgs['fileFullPath'], $packageArgs['fileFullPath64'])
+$filesToRemove | % { Remove-Item -Path $_ -ErrorAction SilentlyContinue -Force }
+
+New-Item `
+  -Path "$toolsDir\r128gain*" `
+  -ItemType File `
+  -Name 'ffmpeg.exe.ignore'
+
+Get-ChildItem "$toolsDir\r128gain*\lib\distutils\command\*" -Filter '*.exe' | % {
+  New-Item `
+    -Path $_.DirectoryName `
+    -ItemType File `
+    -Name "$($_.Name).ignore"
+}
---
+++ r128gain/update.ps1
@@ -0,0 +1,37 @@
+import-module au
+
+function global:au_SearchReplace {
+    @{
+        ".\tools\chocolateyinstall.ps1"   = @{
+            "(?i)(^\s*fileFullPath\s*=\s*)(.*)"    = "`${1}Join-Path `$toolsDir '$($Latest.FileName32)'"
+            "(?i)(^\s*fileFullPath64\s*=\s*)(.*)"  = "`${1}Join-Path `$toolsDir '$($Latest.FileName64)'"
+        }
+        ".\legal\VERIFICATION.txt" = @{
+            "(?i)(\s+x86:).*"          = "`${1} $($Latest.Url32)"
+            "(?i)(\s+x64:).*"          = "`${1} $($Latest.Url64)"
+            "(?i)(\s+checksum32:).*"   = "`${1} $($Latest.Checksum32)"
+            "(?i)(\s+checksum64:).*"   = "`${1} $($Latest.Checksum64)"
+        }
+        ".\r128gain.nuspec" = @{
+            "(?i)(\<releaseNotes\>).*(\<\/releaseNotes\>)" = "`${1}$($Latest.ReleaseNotes)`${2}"
+        }
+    }
+}
+
+function global:au_GetLatest {
+    $download_page = Invoke-WebRequest -UseBasicParsing -Uri 'https://github.com/desbma/r128gain/releases'
+    $relative_url  = $download_page.links | Where-Object href -match '/desbma/r128gain/releases/download/\d+\.\d+(\.\d+)*/r128gain_win64\.7z' | Select-Object -First 1 -expand href
+    $version = ([regex]::Match($relative_url, '/(\d+\.\d+(\.\d+)*)/')).Groups[1].Value
+    @{
+        Version      = $version
+        Url32        = "https://github.com/desbma/r128gain/releases/download/$version/r128gain_win32.7z"
+        Url64        = "https://github.com/desbma/r128gain/releases/download/$version/r128gain_win64.7z"
+        ReleaseNotes = "https://github.com/desbma/r128gain/releases/tag/$version"
+    }
+}
+
+function global:au_BeforeUpdate {
+    Get-RemoteFiles -NoSuffix -Purge
+}
+
+Update-Package -ChecksumFor None
