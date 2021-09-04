381717553e2943ef918e0e765e31f3a4ae29aab8
Jakub LevÃÂ½ <jakub.levy15@gmail.com>
2021-09-04 17:55:47 +0200

    mnemosyne added

---
+++ mnemosyne/tools/chocolateyinstall.ps1
@@ -0,0 +1,25 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir = Split-Path -parent $MyInvocation.MyCommand.Definition
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  fileType       = 'EXE'
+  file           = Join-Path $toolsDir 'mnemosyne-2.8-setup.exe'
+  softwareName   = "$env:ChocolateyPackageName*"
+  silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  validExitCodes = @(0)
+}
+
+$additionalArgs = Get-PackageParameters
+
+if($additionalArgs['InstallDir']) {
+  $path = $additionalArgs['InstallDir']
+  $packageArgs['silentArgs'] += " /DIR=""$path"""
+}
+
+Install-ChocolateyInstallPackage @packageArgs
+
+Remove-Item `
+  -Path $packageArgs['file'] `
+  -ErrorAction SilentlyContinue `
+  -Force
---
+++ mnemosyne/update.ps1
@@ -0,0 +1,37 @@
+import-module au
+
+function global:au_SearchReplace {
+    @{
+        ".\tools\chocolateyinstall.ps1"   = @{
+            "(?i)(^\s*file\s*=\s*)(.*)"   = "`${1}Join-Path `$toolsDir '$($Latest.FileName32)'"
+        }
+        ".\legal\VERIFICATION.txt" = @{
+            "(?i)(\s+Go to).*"          = "`${1} $($Latest.Url32)"
+            "(?i)(\s+checksum32:).*"    = "`${1} $($Latest.Checksum32)"
+        }
+        ".\mnemosyne.nuspec" = @{
+            "(?i)(\<releaseNotes\>).*(\<\/releaseNotes\>)" = "`${1}$($Latest.ReleaseNotes)`${2}"
+        }
+    }
+}
+
+function global:au_GetLatest {
+    $download_page = Invoke-WebRequest -UseBasicParsing -Uri 'https://sourceforge.net/projects/mnemosyne-proj/files/mnemosyne/'
+    $relative_urls  = $download_page.links | ? href -match '/mnemosyne-\d+\.\d+(\.\d+)*/$' | select -exp href
+    $versions = $relative_urls | % { ([regex]::Match($_, '(?i)/mnemosyne-(\d+\.\d+(\.\d+)*)/')).Groups[1].Value }
+    $version = $versions | Sort-Object -Descending {[version] $_ } | select -First 1
+    @{
+        Version          = $version
+        Url32            = "https://sourceforge.net/projects/mnemosyne-proj/files/mnemosyne/mnemosyne-$version/mnemosyne-$version-setup.exe"
+    }
+}
+
+function global:au_BeforeUpdate {
+    Get-RemoteFiles -NoSuffix -Purge
+}
+
+function global:au_AfterUpdate($pkg) {
+    Set-DescriptionFromReadme $pkg
+}
+
+Update-Package -ChecksumFor None
