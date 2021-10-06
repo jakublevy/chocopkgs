06a0b0f71f7b896e585b330f8f6a811159cd068b
Jakub Levý <jakub.levy15@gmail.com>
2021-10-06 20:54:08 +0200

    nuXmv added

---
+++ nuxmv/tools/chocolateyinstall.ps1
@@ -0,0 +1,34 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir              = Split-Path -parent $MyInvocation.MyCommand.Definition
+$version               = '2.0.0'
+$checksum32            = 'C56622505110DC9358BACFB295F5CCFADF64E2F2930E4E7AE4B7FFB58C47D57A'
+$checksum64            = 'DF7A489A7A257BC187D9AECC01368DB6A5B6E57BD0C061FCA385E3E046A8D31E'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  url           = "https://es-static.fbk.eu/tools/nuxmv/downloads/nuXmv-$version-win32.tar.gz"
+  url64         = "https://es-static.fbk.eu/tools/nuxmv/downloads/nuXmv-$version-win64.tar.gz"
+  checksum      = $checksum32
+  checksum64    = $checksum64
+  checksumType  = 'sha256'
+  checksumType64= 'sha256'
+  unzipLocation = $toolsDir
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyZipPackage @packageArgs
+
+$packageArgsUntar = @{
+  packageName   = $env:ChocolateyPackageName
+  fileFullPath  = "$toolsDir\nuXmv-$version-win32.tar"
+  fileFullPath64= "$toolsDir\nuXmv-$version-win64.tar"
+  destination   = $toolsDir
+  validExitCodes= @(0)
+}
+
+Get-ChocolateyUnzip @packageArgsUntar
+
+Remove-Item `
+  -Path "$toolsDir\*.tar" `
+  -ErrorAction SilentlyContinue `
+  -Force
---
+++ nuxmv/update.ps1
@@ -0,0 +1,35 @@
+import-module au
+
+function global:au_SearchReplace {
+    @{
+        ".\tools\chocolateyinstall.ps1"   = @{
+            "(^[$]version\s*=\s*)('.*')"  = "`$1'$($Latest.Version)'"
+            "(^[$]checksum32\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+            "(^[$]checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+        }
+    }
+}
+
+function global:au_GetLatest {
+    $session = New-Object Microsoft.PowerShell.Commands.WebRequestSession
+    $cookie = New-Object System.Net.Cookie 
+    $cookie.Name = "nuXmv"
+    $cookie.Value = "registered"
+    $cookie.Domain = "nuxmv.fbk.eu"
+    $session.Cookies.Add($cookie);
+    $download_page =   Invoke-WebRequest -UseBasicParsing -Uri 'https://nuxmv.fbk.eu/index.php?n=Download.HiddenDownload' -WebSession $session
+    $absolute_url64  = $download_page.links | Where-Object href -match 'nuXmv-\d+\.\d+(\.\d+)*-win64' | Select-Object -First 1 -expand href
+    $absolute_url32  = $absolute_url64.Replace('win64', 'win32')
+    $version = [regex]::Match($absolute_url64, 'nuXmv-(\d+\.\d+(\.\d+)*)-win64').Groups[1].Value
+    @{
+        Version = $version
+        Url64   = $absolute_url64
+        Url32   = $absolute_url32
+    }
+}
+
+function global:au_AfterUpdate($pkg) {
+    Set-DescriptionFromReadme $pkg
+}
+
+Update-Package
