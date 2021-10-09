36aa5d19a81536a2db1f9c5d8cfda3e00bad2b4b
Jakub LevÃÂ½ <jakub.levy15@gmail.com>
2021-10-09 17:58:16 +0200

    file added

---
+++ file/update.ps1
@@ -0,0 +1,42 @@
+import-module au
+
+function global:au_SearchReplace {
+    @{
+        ".\tools\chocolateyinstall.ps1"   = @{
+            "(?i)(^\s*fileFullPath\s*=\s*)(.*)"    = "`${1}Join-Path `$toolsDir '$($Latest.FileName32)'"
+            "(?i)(^\s*fileFullPath64\s*=\s*)(.*)"  = "`${1}Join-Path `$toolsDir '$($Latest.FileName64)'"
+        }
+        ".\legal\VERIFICATION.txt" = @{
+            "(?i)(\s+x86:).*"          = "`${1} $($Latest.Url32)"
+            "(?i)(\s+x64:).*"          = "`${1} $($Latest.Url64)"
+            "(?i)(\s+checksum32:).*"   = "`${1} $($Latest.Checksum32)"
+            "(?i)(\s+checksum64:).*"   = "`${1} $($Latest.Checksum64)"
+        }
+        ".\file.nuspec" = @{
+            "(?i)(\<releaseNotes\>).*(\<\/releaseNotes\>)" = "`${1}$($Latest.ReleaseNotes)`${2}"
+        }
+    }
+}
+
+function global:au_GetLatest {
+    $download_page = Invoke-WebRequest -UseBasicParsing -Uri 'https://github.com/julian-r/file-windows/releases'
+    $relative_url  = $download_page.links | Where-Object href -match '/julian-r/file-windows/releases/download/v\d+\.\d+(\.\d+)*/file.*-vs2019-x64\.zip' | Select-Object -First 1 -expand href
+    $relative32 = $relative_url.Replace('x64', 'x86')
+    $version = ([regex]::Match($relative_url, '/v(\d+\.\d+(\.\d+)*)/')).Groups[1].Value
+    @{
+        Version      = $version
+        Url32        = "https://github.com$relative32"
+        Url64        = "https://github.com$relative_url"
+        ReleaseNotes = "https://github.com/julian-r/file-windows/releases/tag/v$version"
+    }
+}
+
+function global:au_BeforeUpdate {
+    Get-RemoteFiles -NoSuffix -Purge
+}
+
+function global:au_AfterUpdate($pkg) {
+    Set-DescriptionFromReadme $pkg
+}
+
+Update-Package -ChecksumFor None
