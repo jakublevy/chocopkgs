60d765802c7689748f6da4240513b58fd057257b
Jakub Levý <jakub.levy15@gmail.com>
2021-10-09 17:58:16 +0200

    file added

---
+++ file/tools/chocolateyinstall.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir = Split-Path -parent $MyInvocation.MyCommand.Definition
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  fileFullPath   = Join-Path $toolsDir 'file_5.38-build49-vs2019-x86.zip'
+  fileFullPath64 = Join-Path $toolsDir 'file_5.38-build49-vs2019-x64.zip'
+  destination    = "$toolsDir\binaries"
+  validExitCodes = @(0)
+}
+
+Get-ChocolateyUnzip @packageArgs
+
+$filesToRemove = @($packageArgs['fileFullPath'], $packageArgs['fileFullPath64'])
+$filesToRemove | % { Remove-Item -Path $_ -ErrorAction SilentlyContinue -Force }
