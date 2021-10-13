010b5cb3fa15d5b59e0a7730cfc13cfb731f6981
Jakub LevÃÂ½ <jakub.levy15@gmail.com>
2021-10-13 14:37:33 +0200

    spin installation parameters added

--- spin/tools/chocolateyinstall.ps1
+++ spin/tools/chocolateyinstall.ps1
@@ -14,3 +14,25 @@ Remove-Item `
   -Path $packageArgs['fileFullPath64'] `
   -ErrorAction SilentlyContinue `
   -Force
+
+$shortcutArgs = @{
+  targetPath  = "$toolsDir\bin\ispin.tcl"
+}
+
+$additionalArgs = Get-PackageParameters
+if($additionalArgs['CreateiSpinDesktopIcon']) {
+  Install-ChocolateyShortcut `
+    -ShortcutFilePath "$env:userprofile\Desktop\iSpin.lnk" `
+    @shortcutArgs
+}
+
+if($additionalArgs['AddiSpinToStartMenu'] -eq "allusers") {
+  Install-ChocolateyShortcut `
+    -ShortcutFilePath "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\iSpin.lnk" `
+    @shortcutArgs
+}
+elseif($additionalArgs['AddiSpinToStartMenu'] -eq "curruser") {
+  Install-ChocolateyShortcut `
+    -ShortcutFilePath "$env:AppData\Microsoft\Windows\Start Menu\Programs\iSpin.lnk" `
+    @shortcutArgs
+}
