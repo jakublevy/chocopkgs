9c153d2fb338452ccd0499b9974c6bd95ee0954a
Jakub Levý <jakub.levy15@gmail.com>
2021-10-17 09:24:06 +0200

    spicetify added 32-bit support

--- spicetify-cli/update.ps1
+++ spicetify-cli/update.ps1
@@ -3,11 +3,14 @@ import-module au
 function global:au_SearchReplace {
     @{
         ".\tools\chocolateyinstall.ps1"   = @{
+            "(?i)(^\s*fileFullPath\s*=\s*)(.*)"    = "`${1}Join-Path `$toolsDir '$($Latest.FileName32)'"
             "(?i)(^\s*fileFullPath64\s*=\s*)(.*)"  = "`${1}Join-Path `$toolsDir '$($Latest.FileName64)'"
         }
         ".\legal\VERIFICATION.txt" = @{
-            "(?i)(\s+Go to).*"          = "`${1} $($Latest.Url64)"
-            "(?i)(\s+checksum64:).*"    = "`${1} $($Latest.Checksum64)"
+            "(?i)(\s+x86:).*"          = "`${1} $($Latest.Url32)"
+            "(?i)(\s+x64:).*"          = "`${1} $($Latest.Url64)"
+            "(?i)(\s+checksum32:).*"   = "`${1} $($Latest.Checksum32)"
+            "(?i)(\s+checksum64:).*"   = "`${1} $($Latest.Checksum64)"
         }
         ".\spicetify-cli.nuspec" = @{
             "(?i)(\<releaseNotes\>).*(\<\/releaseNotes\>)" = "`${1}$($Latest.ReleaseNotes)`${2}"
@@ -21,6 +24,7 @@ function global:au_GetLatest {
     $version = ([regex]::Match($relative_url, 'v(\d+\.\d+(\.\d+)*)')).Groups[1].Value
     @{
         Version      = $version
+        Url32        = "https://github.com/khanhas/spicetify-cli/releases/download/v$version/spicetify-$version-windows-x32.zip"
         Url64        = "https://github.com/khanhas/spicetify-cli/releases/download/v$version/spicetify-$version-windows-x64.zip"
         ReleaseNotes = "https://github.com/khanhas/spicetify-cli/releases/tag/v$version"
     }
