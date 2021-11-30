354f59adc6afd84826dc1d634934313d6e188a6c
Jakub Levý <jakub.levy15@gmail.com>
2021-11-30 23:22:26 +0100

    visualgdb updated to 5.6.2

--- visualgdb/tools/chocolateyinstall.ps1
+++ visualgdb/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop'
-$absUrl   = 'https://sysprogs.com/getfile/1686/VisualGDB-5.6-trial.msi'
-$checksum = 'DC1E1AE3CD58D94C73198FFD715EF06FF1468B5D32AFE93127D8A5DFC3C5A0D0'
+$absUrl   = 'https://sysprogs.com/getfile/1762/VisualGDB-5.6r2-trial.msi'
+$checksum = '58193bc70daa4626e51236f6aa6ba402e914fd679da87ad6e6dea25309f4f12b'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
