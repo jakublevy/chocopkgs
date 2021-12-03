acb20ad7045781fc77eb3c8e98a9c49b0be6b867
Jakub Levý <jakub.levy15@gmail.com>
2021-12-03 16:52:09 +0100

    mairix added

---
+++ mairix/tools/chocolateyinstall.ps1
@@ -0,0 +1,16 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  fileFullPath  = Join-Path $toolsDir 'mairix-0.22-w32-bin.zip'
+  destination   = $toolsDir
+  validExitCodes= @(0)
+}
+
+Get-ChocolateyUnzip @packageArgs
+
+Remove-Item `
+  -Path $packageArgs['fileFullPath'] `
+  -ErrorAction SilentlyContinue `
+  -Force
