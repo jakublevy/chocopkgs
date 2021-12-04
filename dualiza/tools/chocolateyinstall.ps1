ce42052b2efcaf32936ef69ddd55d28f89ee1177
Jakub LevÃÂ½ <jakub.levy15@gmail.com>
2021-12-04 09:44:34 +0100

    dualiza added

---
+++ dualiza/tools/chocolateyinstall.ps1
@@ -0,0 +1,16 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  fileFullPath64= Join-Path $toolsDir 'dualiza-1.0.0-win-x64.zip'
+  destination   = $toolsDir
+  validExitCodes= @(0)
+}
+
+Get-ChocolateyUnzip @packageArgs
+
+Remove-Item `
+  -Path $packageArgs['fileFullPath64'] `
+  -ErrorAction SilentlyContinue `
+  -Force
