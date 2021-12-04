4f9a687465e674257839269cf8592faf94278981
Jakub Levý <jakub.levy15@gmail.com>
2021-12-04 09:44:34 +0100

    dualiza added

---
+++ dualiza/README.md
@@ -0,0 +1,8 @@
+# Dualiza
+Dualiza is a model counter, model enumerator and SAT solver. It works with DIMACS and AIGER files as well as plain formulas.
+
+## Additional resources
+* [Docs](https://github.com/arminbiere/dualiza/tree/master/doc)
+* [AIG examples](https://github.com/arminbiere/dualiza/tree/master/aigs)
+* [CNF examples](https://github.com/arminbiere/dualiza/tree/master/cnfs)
+* [Formulae examples](https://github.com/arminbiere/dualiza/tree/master/formulas)
---
+++ dualiza/update.ps1
@@ -0,0 +1,35 @@
+import-module au
+
+function global:au_SearchReplace {
+    @{
+        ".\tools\chocolateyinstall.ps1"   = @{
+            "(?i)(^\s*fileFullPath64\s*=\s*)(.*)" = "`${1}Join-Path `$toolsDir '$($Latest.FileName64)'"
+        }
+        ".\legal\VERIFICATION.txt" = @{
+            "(?i)(\s+Go to).*"         = "`${1} $($Latest.Url64)"
+            "(?i)(\s+checksum64:).*"   = "`${1} $($Latest.Checksum64)"
+        }
+        ".\dualiza.nuspec" = @{
+            "(?i)(\<releaseNotes\>).*(\<\/releaseNotes\>)" = "`${1}$($Latest.ReleaseNotes)`${2}"
+        }
+    }
+}
+
+function global:au_GetLatest {
+    $response = Invoke-WebRequest -UseBasicParsing -Uri 'https://raw.githubusercontent.com/arminbiere/dualiza/master/VERSION'
+    $version = $response.Content.Trim()
+    @{
+        Url64        = "https://github.com/jakublevy/dualiza/releases/download/v$version/dualiza-$version-win-x64.zip"
+        Version      = $version
+        ReleaseNotes = "https://github.com/jakublevy/dualiza/releases/tag/v$version"
+    }
+}
+function global:au_BeforeUpdate {
+    Get-RemoteFiles -Purge -NoSuffix
+}
+
+function global:au_AfterUpdate($pkg) {
+    Set-DescriptionFromReadme $pkg
+}
+
+Update-Package -ChecksumFor None
