5b2509a6de2122bbb113cfc027e7f6c0b56bf191
Jakub Levý <jakub.levy15@gmail.com>
2021-12-06 09:39:19 +0100

    ddcpuid added

---
+++ ddcpuid/tools/chocolateyinstall.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir = Split-Path -parent $MyInvocation.MyCommand.Definition
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  fileFullPath   = Join-Path $toolsDir 'ddcpuid-0.19.1-windows-msvc-x86.zip'
+  fileFullPath64 = Join-Path $toolsDir 'ddcpuid-0.19.1-windows-msvc-x86_64.zip'
+  destination    = "$toolsDir\bin"
+  validExitCodes = @(0)
+}
+
+Get-ChocolateyUnzip @packageArgs
+
+$filesToRemove = @($packageArgs['fileFullPath'], $packageArgs['fileFullPath64'])
+$filesToRemove | % { Remove-Item -Path $_ -ErrorAction SilentlyContinue -Force }
