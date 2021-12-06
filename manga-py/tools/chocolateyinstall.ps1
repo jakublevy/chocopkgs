8a86c0cc1be649d7324624631e325fac01fb442b
Jakub LevÃÂ½ <jakub.levy15@gmail.com>
2021-12-06 19:40:59 +0100

    manga-py added

---
+++ manga-py/tools/chocolateyinstall.ps1
@@ -0,0 +1,16 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  fileFullPath64= Join-Path $toolsDir 'manga_py-1.32.0-win-x64.zip'
+  destination   = $toolsDir
+  validExitCodes= @(0)
+}
+
+Get-ChocolateyUnzip @packageArgs
+
+Remove-Item `
+  -Path $packageArgs['fileFullPath64'] `
+  -ErrorAction SilentlyContinue `
+  -Force
---
+++ manga-py/update.ps1
@@ -0,0 +1,36 @@
+import-module au
+
+function global:au_SearchReplace {
+    @{
+        ".\tools\chocolateyinstall.ps1"   = @{
+            "(?i)(^\s*fileFullPath64\s*=\s*)(.*)" = "`${1}Join-Path `$toolsDir '$($Latest.FileName64)'"
+        }
+        ".\legal\VERIFICATION.txt" = @{
+            "(?i)(\s+Go to).*"         = "`${1} $($Latest.Url64)"
+            "(?i)(\s+checksum64:).*"   = "`${1} $($Latest.Checksum64)"
+        }
+        ".\manga-py.nuspec" = @{
+            "(?i)(\<releaseNotes\>).*(\<\/releaseNotes\>)" = "`${1}$($Latest.ReleaseNotes)`${2}"
+        }
+    }
+}
+
+function global:au_GetLatest {
+    $download_page = Invoke-WebRequest -UseBasicParsing -Uri 'https://github.com/manga-py/manga-py/releases'
+    $relative_url  = $download_page.links | Where-Object href -match '/manga-py/manga-py/archive/refs/tags/\d+\.\d+(\.\d+)\.zip' | Select-Object -First 1 -expand href
+    $version = ([regex]::Match($relative_url, '(\d+\.\d+(\.\d+)*)\.zip')).Groups[1].Value
+    @{
+        Version      = $version
+        Url64        = "https://github.com/jakublevy/manga-py-win/releases/download/v$version/manga_py-$version-win-x64.zip"
+        ReleaseNotes = "https://github.com/manga-py/manga-py/releases/tag/$version"
+    }
+}
+function global:au_BeforeUpdate {
+    Get-RemoteFiles -Purge -NoSuffix
+}
+
+function global:au_AfterUpdate($pkg) {
+    Set-DescriptionFromReadme $pkg
+}
+
+Update-Package -ChecksumFor None
