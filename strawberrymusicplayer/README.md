1fda6f691196b0c51b2f6a164313b45e0d735d9a
Jakub LevÃÂ½ <jakub.levy15@gmail.com>
2021-12-07 14:28:29 +0100

    strawberrymusicplayer added

---
+++ strawberrymusicplayer/README.md
@@ -0,0 +1,12 @@
+# Strawberry Music Player
+Strawberry is a music player and music collection organizer. It is a fork of Clementine released in 2018 aimed at music collectors and audiophiles. It's written in C++ using the Qt toolkit.
+
+## Installation Parameters
+* `/InstallDir:` - where to install the binaries
+    - Default value: `"$env:ProgramFiles\Strawberry Music Player"`
+
+### Examples
+* Install into `D:\strawberry-music-player` directory
+    ```
+    choco install strawberrymusicplayer --params "/InstallDir:D:\strawberry-music-player"
+    ```
---
+++ strawberrymusicplayer/tools/chocolateyinstall.ps1
@@ -0,0 +1,22 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir = Split-Path -parent $MyInvocation.MyCommand.Definition
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  fileType      = 'EXE'
+  file          = Join-Path $toolsDir 'StrawberrySetup-1.0.0-Qt6-x86.exe'
+  file64        = Join-Path $toolsDir 'StrawberrySetup-1.0.0-Qt6-x64.exe'
+  softwareName  = 'Strawberry Music Player*'
+  silentArgs    = "/S"
+  validExitCodes= @(0)
+}
+
+$additionalArgs = Get-PackageParameters
+if($additionalArgs['InstallDir']) {
+  $packageArgs['silentArgs'] += " /D=$($additionalArgs['InstallDir'])"
+}
+
+Install-ChocolateyInstallPackage @packageArgs
+
+$filesToRemove = @($packageArgs['file'], $packageArgs['file64'])
+$filesToRemove | % { Remove-Item -Path $_ -ErrorAction SilentlyContinue -Force }
