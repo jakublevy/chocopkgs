49527e40b8ecc190de77963b25df7c053bf80d50
Jakub LevÃÂ½ <jakub.levy15@gmail.com>
2021-12-10 19:10:22 +0100

    pdftk-java java path updated

--- pdftk-java/tools/chocolateyinstall.ps1
+++ pdftk-java/tools/chocolateyinstall.ps1
@@ -10,7 +10,10 @@ $additionalArgs = Get-PackageParameters
 if($additionalArgs['JavaExePath']) {
   $binFileArgs['path'] = $additionalArgs['JavaExePath']
 }
-elseif ($null -ne $env:JAVA_HOME) { 
+elseif(Get-Command "$env:ChocolateyInstall\bin\java.exe" -ErrorAction SilentlyContinue) {
+  $binFileArgs['path'] = 'java.exe' # relative path from "$env:ChocolateyInstall\bin" is allowed
+}
+elseif ($null -ne $env:JAVA_HOME) {  
   $binFileArgs['path'] = "$env:JAVA_HOME\bin\java.exe"
 }
 else {
