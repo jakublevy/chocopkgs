fd61c1e879a2b625d454172b82a02b91ec31970d
Jakub Levý <jakub.levy15@gmail.com>
2021-12-11 09:34:57 +0100

    quiet added

---
+++ quiet/README.md
@@ -0,0 +1,9 @@
+# Quiet
+Command line tool to silently launch a process. Similar to `nohup cmd &amp;` on UNIX.
+
+## Usage
+```
+quiet "path" "arguments"
+
+quiet mpv "video.mkv"
+```
---
+++ quiet/tools/chocolateyinstall.ps1
@@ -0,0 +1,35 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir              = Split-Path -parent $MyInvocation.MyCommand.Definition
+$checksum32            = '1CAE914E43B39E47261AAFCB5F6A9C3F317EFE4757046720AB8D8F61D16C6AFA'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  file          = "$toolsDir\Quiet.zip"
+  checksum      = $checksum32
+  checksumType  = 'sha256'
+  unzipLocation = "$toolsDir\bin"
+  validExitCodes= @(0)
+}
+
+$requestParams = @{
+  useBasicParsing = $true
+  uri             = 'https://www.joeware.net/downloads/dl.php'
+  method          = 'POST'
+  outFile         = "$toolsDir\Quiet.zip"
+  timeoutSec      = 180
+  body            = @{
+    download = 'Quiet.zip'
+    B1       = 'Download+Now'
+  }
+}
+
+Write-Host "Please be patient when installing this package, it can take up to 3 minutes due to a slow response of the download server." -ForegroundColor Cyan
+
+Invoke-WebRequest @requestParams
+
+Install-ChocolateyZipPackage @packageArgs
+
+Remove-Item `
+  -Path "$toolsDir\Quiet.zip" `
+  -ErrorAction SilentlyContinue `
+  -Force
