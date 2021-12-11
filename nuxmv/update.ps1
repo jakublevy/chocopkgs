674530e5e6d5598a1807e05488a3913778dffe24
Jakub Levý <jakub.levy15@gmail.com>
2021-12-12 00:00:17 +0100

    nuxmv update.ps1 fixed for pwsh

--- nuxmv/update.ps1
+++ nuxmv/update.ps1
@@ -18,7 +18,13 @@ function global:au_GetLatest {
     $cookie.Domain = "nuxmv.fbk.eu"
     $session.Cookies.Add($cookie);
     $download_page =   Invoke-WebRequest -UseBasicParsing -Uri 'https://nuxmv.fbk.eu/index.php?n=Download.HiddenDownload' -WebSession $session
-    $absolute_url64  = $download_page.links | Where-Object href -match 'nuXmv-\d+\.\d+(\.\d+)*-win64' | Select-Object -First 1 -expand href
+    if($PSVersionTable.PSEdition -eq 'Core') {
+        $content = [system.text.encoding]::UTF8.GetString($download_page.Content)
+        $absolute_url64 = [regex]::Match($content, "href=.*(https://.*nuXmv-\d+\.\d+(\.\d+)*-win64.tar\.gz)").Groups[1].Value
+    }
+    else {
+        $absolute_url64  = $download_page.links | Where-Object href -match 'nuXmv-\d+\.\d+(\.\d+)*-win64' | Select-Object -First 1 -expand href
+    }
     $absolute_url32  = $absolute_url64.Replace('win64', 'win32')
     $version = [regex]::Match($absolute_url64, 'nuXmv-(\d+\.\d+(\.\d+)*)-win64').Groups[1].Value
     @{
