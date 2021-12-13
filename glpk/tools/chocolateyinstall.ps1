37b1dae5ec11b61aba9ea8b61937a82972ecd664
Jakub Levý <jakub.levy15@gmail.com>
2021-12-13 21:57:09 +0100

    Write-Output -> Write-Host

--- glpk/tools/chocolateyinstall.ps1
+++ glpk/tools/chocolateyinstall.ps1
@@ -22,7 +22,7 @@ if((Get-OSArchitectureWidth -Compare 32) -or $env:ChocolateyForceX86) {
 
 $installedDir = (Get-ChildItem "$($packageArgs['destination'])\glpk*")[0].FullName
 $parentKeepList = @("w$bits", 'doc', 'examples', 'NEWS', 'README', 'README.md', 'THANKS', 'COPYING', 'INSTALL', 'AUTHORS', 'ChangeLog')
-Write-Output 'Removing unnecessary compilation left-overs.'
+Write-Host 'Removing unnecessary compilation left-overs.'
 Get-ChildItem $installedDir | ? { $parentKeepList -notcontains $_.Name } | Remove-Item -Recurse -Force
 Get-ChildItem "$installedDir\doc" -Exclude '*.pdf', '*.txt' , 'cli', 'notes' | Remove-Item -Force
 Get-ChildItem "$installedDir\$($parentKeepList[0])" -Exclude '*.jar', '*.exe', '*.txt', '*.dll' | Remove-Item -Force
