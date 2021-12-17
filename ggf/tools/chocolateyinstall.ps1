51379861527775a56ea1d7271c013cf95e3a37aa
Jakub LevÃÂ½ <jakub.levy15@gmail.com>
2021-12-17 22:53:17 +0100

    ggf added

---
+++ ggf/tools/chocolateyinstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir = Split-Path -parent $MyInvocation.MyCommand.Definition
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  fileFullPath   = Join-Path $toolsDir 'ggf-win32.zip'
+  fileFullPath64 = Join-Path $toolsDir 'ggf-win64.zip'
+  destination    = "$toolsDir\bin"
+  validExitCodes = @(0)
+}
+
+Get-ChocolateyUnzip @packageArgs
+
+Remove-Item `
+  -Path $packageArgs['fileFullPath'], $packageArgs['fileFullPath64'] `
+  -ErrorAction SilentlyContinue `
+  -Force
