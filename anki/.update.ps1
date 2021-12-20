8d528ef7ddc6e09f7cf6390334cf26733739f31c
Jakub Levý <jakub.levy15@gmail.com>
2021-12-20 11:32:39 +0100

    anki updated to match Lite's repo

--- anki/.update.ps1
+++ anki/.update.ps1
@@ -2,31 +2,27 @@ import-module au
 
 function global:au_SearchReplace {
     @{
-        ".\tools\chocolateyinstall.ps1"   = @{
-            "(?i)(^\s*file\s*=\s*)(.*)"   = "`${1}Join-Path `$toolsDir '$($Latest.FileName32)'"
-        }
-        ".\legal\VERIFICATION.txt" = @{
-            "(?i)(\s+Go to).*"         = "`${1} $($Latest.Url32)"
-            "(?i)(\s+checksum32:).*"   = "`${1} $($Latest.Checksum32)"
+        ".\tools\chocolateyInstall.ps1" = @{
+            "(^[$]version\s*=\s*)('.*')"  = "`$1'$($Latest.Version)'"
+            "(^[$]checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
         }
         ".\anki.nuspec" = @{
-            "(?i)(\<releaseNotes\>).*(\<\/releaseNotes\>)" = "`${1}$($Latest.ReleaseNotes)`${2}"
+            "(?i)(<releaseNotes>).*(</releaseNotes>)" = "`${1}$($Latest.ReleaseNotes)`${2}"
         }
     }
 }
 
 function global:au_GetLatest {
-    $download_page = Invoke-WebRequest -UseBasicParsing -Uri 'https://github.com/ankitects/anki/releases'
-    $relative_url  = $download_page.links | Where-Object href -match '/ankitects/anki/releases/download/\d+\.\d+(\.\d+)*/anki-\d+\.\d+(\.\d+)*-windows\.exe' | Select-Object -First 1 -expand href
-    $version = ([regex]::Match($relative_url, 'download/(\d+\.\d+(\.\d+)*)/anki-\d+\.\d+(\.\d+)*-windows\.exe')).Groups[1].Value
+    $download_page = Invoke-WebRequest -UseBasicParsing -Uri 'https://apps.ankiweb.net/'
+
+    $url = $download_page.links | Where-Object href -match 'https://github.com/ankitects/anki/releases/download/\d+\.\d+\.\d+/anki-\d+\.\d+\.\d+-windows.exe' | Select-Object -First 1 -expand href
+    $version = ([regex]::Match($url, '(\d+\.\d+\.\d+)-windows.exe')).Groups[1].Value
+
     @{
-        Url32        = "https://github.com/ankitects/anki/releases/download/$version/anki-$version-windows.exe"
+        URL32        = $url
         Version      = $version
         ReleaseNotes = "https://github.com/ankitects/anki/releases/tag/$version"
     }
 }
-function global:au_BeforeUpdate {
-    Get-RemoteFiles -Purge -NoSuffix
-}
 
-Update-Package -ChecksumFor None
+Update-Package -ChecksumFor 32
--- anki/tools/chocolateyinstall.ps1
+++ anki/tools/chocolateyinstall.ps1
@@ -1,43 +1,37 @@
 ﻿$ErrorActionPreference = 'Stop'
-$toolsDir              = Split-Path -parent $MyInvocation.MyCommand.Definition
 $installDir            = "$env:ProgramFiles\Anki"
-
-$packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
-  fileType      = 'EXE'
-  file          = Join-Path $toolsDir 'anki-2.1.49-windows.exe'
-  softwareName  = 'Anki*'
-  silentArgs    = "/S"
-  validExitCodes= @(0)
-}
+$checksum              = '3c9764cb4746cfa4059633678b9dcdc0ae5754e61d99855cb0c40fa6bfd33f5e'
+$version               = '2.1.49'
+$silentArgs            = '/S'
 
 $additionalArgs = Get-PackageParameters
 if($additionalArgs['InstallDir']) {
   $installDir = $additionalArgs['InstallDir']
-  $packageArgs['silentArgs'] += " /D=$installDir"
+  $silentArgs += " /D=$installDir"
 }
 
-Install-ChocolateyPackage @packageArgs
-
-Remove-Item `
-  -Path $packageArgs['file'] `
-  -ErrorAction SilentlyContinue `
-  -Force
+Install-ChocolateyPackage `
+	-PackageName 'anki' `
+	-FileType 'exe' `
+	-SilentArgs $silentArgs  `
+	-Url "https://github.com/ankitects/anki/releases/download/$version/anki-$version-windows.exe" `
+	-Checksum $checksum `
+	-ChecksumType 'sha256'
 
 if(!$additionalArgs['CreateDesktopIcon']) {
-  Remove-Item `
-    -Path "$env:Public\Desktop\Anki.lnk" `
-    -Force `
-    -ErrorAction SilentlyContinue
+	Remove-Item `
+		-Path "$env:Public\Desktop\Anki.lnk" `
+		-Force `
+		-ErrorAction SilentlyContinue
 }
-
+	
 # Fix bugged start menu icon
 Remove-Item `
-  -Path "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\Anki.lnk" `
-  -Force `
-  -ErrorAction SilentlyContinue
+	-Path "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\Anki.lnk" `
+	-Force `
+	-ErrorAction SilentlyContinue
 
 Install-ChocolateyShortcut `
-  -ShortcutFilePath "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\Anki.lnk" `
-  -TargetPath "$installDir\anki.exe" `
-  -WorkingDirectory $installDir
+	-ShortcutFilePath "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\Anki.lnk" `
+	-TargetPath "$installDir\anki.exe" `
+	-WorkingDirectory $installDir
