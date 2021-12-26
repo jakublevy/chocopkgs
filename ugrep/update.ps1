80ccfd6c5063b3e49c426ece7a37af9180b46591
Jakub Levý <jakub.levy15@gmail.com>
2021-12-26 19:09:45 +0100

    ugrep added

---
+++ ugrep/update.ps1
@@ -0,0 +1,25 @@
+import-module au
+
+function global:au_SearchReplace {
+    @{
+        ".\legal\VERIFICATION.txt" = @{
+            "(?i)(\s+Go to).*"         = "`${1} $($Latest.Url64)"
+            "(?i)(\s+checksum64:).*"   = "`${1} $($Latest.Checksum64)"
+        }
+    }
+}
+
+function global:au_GetLatest {
+    $download_page = Invoke-WebRequest -UseBasicParsing -Uri 'https://github.com/Genivia/ugrep/releases'
+    $relative_url  = $download_page.links | Where-Object href -match '/Genivia/ugrep/releases/download/v\d+\.\d+(\.\d+)*/ugrep\.exe' | Select-Object -First 1 -expand href
+    $version = ([regex]::Match($relative_url, '/v(\d+\.\d+(\.\d+)*)/')).Groups[1].Value
+    @{
+        Url64        = "https://github.com/Genivia/ugrep/releases/download/v$version/ugrep.exe"
+        Version      = $version
+    }
+}
+function global:au_BeforeUpdate {
+    Get-RemoteFiles -Purge -NoSuffix
+}
+
+Update-Package -ChecksumFor None
