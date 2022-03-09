5942e7463006790c667add708fe934dd2d8e1a08
Jakub Levý <jakub.levy15@gmail.com>
2022-03-09 12:51:51 +0100

    steghide added

---
+++ steghide/tools/chocolateyinstall.ps1
@@ -0,0 +1,21 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  fileFullPath  = Join-Path $toolsDir 'steghide-0.5.1-win32.zip'
+  destination   = $toolsDir
+  validExitCodes= @(0)
+}
+
+Get-ChocolateyUnzip @packageArgs
+
+Rename-Item `
+  -Path "$toolsDir\steghide" `
+  -NewName 'bin' `
+  -Force
+
+Remove-Item `
+  -Path $packageArgs['fileFullPath'] `
+  -ErrorAction SilentlyContinue `
+  -Force
---
+++ steghide/update.ps1
@@ -0,0 +1,30 @@
+import-module au
+
+function global:au_SearchReplace {
+    @{  
+        ".\tools\chocolateyinstall.ps1"   = @{
+            "(?i)(^\s*fileFullPath\s*=\s*)(.*)"  = "`${1}Join-Path `$toolsDir '$($Latest.FileName32)'"
+        }
+        ".\legal\VERIFICATION.txt" = @{
+            "(?i)(\s+checksum32:).*"     = "`${1} $($Latest.Checksum32)"
+            "(?i)(\s+Go to).*"           = "`${1} $($Latest.Url32)"
+        }
+        ".\steghide.nuspec" = @{
+            "(?i)(\<projectSourceUrl\>).*(\<\/projectSourceUrl\>)" = "`${1}$($Latest.ProjectSrcUrl)`${2}"
+        }
+    }
+}
+
+function global:au_GetLatest {
+    $download_page = Invoke-WebRequest -UseBasicParsing -Uri 'https://sourceforge.net/projects/steghide/files/steghide'
+    $relative_urls  = $download_page.links | ? href -match ".*/\d+\.\d+(\.\d+)*/$" | Select-Object -exp href
+    $versions = $relative_urls | % { ([regex]::Match($_, '.*/(\d+\.\d+(\.\d+)*)/$')).Groups[1].Value }
+    $version = $versions | Sort-Object -Descending {[version] $_ } | Select-Object -First 1
+    @{
+        Url32        = "https://sourceforge.net/projects/steghide/files/steghide/$version/steghide-$version-win32.zip"
+        Version      = $version
+        ProjectSrcUrl= "https://sourceforge.net/projects/steghide/files/steghide/$version/steghide-$version.tar.gz"
+    }
+}
+
+Update-Package -ChecksumFor None
