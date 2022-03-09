6bb8fc9c632b9cf3161e7bd241ae0dfa0992fd29
Jakub Levý <jakub.levy15@gmail.com>
2022-03-09 16:25:55 +0100

    clipstudio-paint fixed update

--- clipstudio-paint/update.ps1
+++ clipstudio-paint/update.ps1
@@ -17,19 +17,9 @@ function global:au_GetLatest {
     # $links = $download_page.links | ? href -match '\.exe' | Select-Object -first 1 -exp href
     # $versionOriginal = [regex]::Match($links, '/(\d+)/').Groups[1].Value
     $releaseNotesPage = Invoke-WebRequest -UseBasicParsing -Uri 'https://www.clipstudio.net/en/dl/release_note'
-    $content = $releaseNotesPage.Content.Substring($releaseNotesPage.Content.IndexOf('<body'))
-    $possibleVersions = [regex]::Matches($content, '(\d+\.\d+(\.\d+)*)') | ? { $_.Success } | % { $_.Groups[1].Value } | Select-Object -Unique | Sort-Object -Descending {[version] $_ }
-    foreach($pv in $possibleVersions) {
-        try{
-            $versionNoDot = $pv.Replace('.', '')
-            Invoke-WebRequest -UseBasicParsing -Uri "https://vd.clipstudio.net/clipcontent/paint/app/$versionNoDot/CSP_${versionNoDot}w_setup.exe" -Method Head
-            $versionChoco = $pv
-            break
-        }
-        catch {
-
-        }
-    }
+    $content = $releaseNotesPage.Content.Substring($releaseNotesPage.Content.IndexOf('<h1'))
+    $versionChoco = [regex]::Matches($content, '(\d+\.\d+(\.\d+)*)') | ? { $_.Success } | % { $_.Groups[1].Value } | Select-Object -First 1
+    $versionNoDot =  $versionChoco.Replace('.', '')
     @{
         Version      = $versionChoco
         VersionClip  = $versionNoDot
