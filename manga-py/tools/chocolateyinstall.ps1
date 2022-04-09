59327848c5f20b62ddc22a0d24c4728df3d24daa
Chocolatey Packages <chocopkgs@gmail.com>
2022-04-10 00:37:12 +0200

    AU: 1 updated - manga-py [skip ci]

--- manga-py/tools/chocolateyinstall.ps1
+++ manga-py/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@ $toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
-  fileFullPath64= Join-Path $toolsDir 'manga_py-1.33.2-win-x64.zip'
+  fileFullPath64= Join-Path $toolsDir 'manga_py-1.33.3-win-x64.zip'
   destination   = $toolsDir
   validExitCodes= @(0)
 }
