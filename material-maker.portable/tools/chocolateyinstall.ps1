3593dbbc7d981c194966a7b90467637047471066
Chocolatey Packages <chocopkgs@gmail.com>
2022-04-11 00:06:41 +0200

    AU: 3 updated - material-maker material-maker.portable strawberrymusicplayer [skip ci]

--- material-maker.portable/tools/chocolateyinstall.ps1
+++ material-maker.portable/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@ $toolsDir = Split-Path -parent $MyInvocation.MyCommand.Definition
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
-  fileFullPath64 = Join-Path $toolsDir 'material_maker_0_98_windows.zip'
+  fileFullPath64 = Join-Path $toolsDir 'material_maker_0_99_windows.zip'
   destination    = $toolsDir
   validExitCodes = @(0)
 }
