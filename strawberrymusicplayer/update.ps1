02816eb3be1c7c484021c0628ef2efdfb1ab7ae2
Jakub Levý <jakub.levy15@gmail.com>
2022-06-12 18:55:06 +0200

    strawberrymusicplayer 32-bit dropped

--- strawberrymusicplayer/update.ps1
+++ strawberrymusicplayer/update.ps1
@@ -3,13 +3,10 @@ import-module au
 function global:au_SearchReplace {
     @{
         ".\tools\chocolateyinstall.ps1"   = @{
-            "(?i)(^\s*file\s*=\s*)(.*)"  = "`${1}Join-Path `$toolsDir '$($Latest.FileName32)'"
             "(?i)(^\s*file64\s*=\s*)(.*)"  = "`${1}Join-Path `$toolsDir '$($Latest.FileName64)'"
         }
         ".\legal\VERIFICATION.txt" = @{
-            "(?i)(\s+x86:).*"          = "`${1} $($Latest.Url32)"
-            "(?i)(\s+x64:).*"          = "`${1} $($Latest.Url64)"
-            "(?i)(\s+checksum32:).*"   = "`${1} $($Latest.Checksum32)"
+            "(?i)(\s+Go to).*"         = "`${1} $($Latest.Url64)"
             "(?i)(\s+checksum64:).*"   = "`${1} $($Latest.Checksum64)"
         }
         ".\strawberrymusicplayer.nuspec" = @{
@@ -24,7 +21,6 @@ function global:au_GetLatest {
     $version = ([regex]::Match($relative_url, '/(\d+\.\d+(\.\d+)*)/')).Groups[1].Value
     @{
         Version      = $version
-        Url32        = "https://github.com/strawberrymusicplayer/strawberry/releases/download/$version/StrawberrySetup-$version-mingw-x86.exe"
         Url64        = "https://github.com/strawberrymusicplayer/strawberry/releases/download/$version/StrawberrySetup-$version-msvc-x64.exe"
         ReleaseNotes = "https://github.com/strawberrymusicplayer/strawberry/blob/$version/Changelog"
     }
