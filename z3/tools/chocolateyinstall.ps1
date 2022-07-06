9ca8b1495136dbac8f1f4eaae111dc6632134615
Chocolatey Packages <chocopkgs@gmail.com>
2022-07-07 00:15:03 +0200

    AU: 1 updated - z3 [skip ci]

--- z3/tools/chocolateyinstall.ps1
+++ z3/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@ $toolsDir = Split-Path -parent $MyInvocation.MyCommand.Definition
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
-  fileFullPath   = Join-Path $toolsDir 'z3-4.8.17-x86-win.zip'
-  fileFullPath64 = Join-Path $toolsDir 'z3-4.8.17-x64-win.zip'
+  fileFullPath   = Join-Path $toolsDir 'z3-4.9.1-x86-win.zip'
+  fileFullPath64 = Join-Path $toolsDir 'z3-4.9.1-x64-win.zip'
   destination    = $toolsDir
   validExitCodes = @(0)
 }
