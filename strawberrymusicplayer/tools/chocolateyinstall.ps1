748f7313c84c5ae37b8f113d2293e801ca684705
Jakub Levý <jakub.levy15@gmail.com>
2022-12-07 22:58:43 +0100

    strawberrymusicplayer fix upgrade

--- strawberrymusicplayer/tools/chocolateyinstall.ps1
+++ strawberrymusicplayer/tools/chocolateyinstall.ps1
@@ -4,6 +4,7 @@ $toolsDir = Split-Path -parent $MyInvocation.MyCommand.Definition
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   fileType      = 'EXE'
+  file          = Join-Path $toolsDir 'StrawberrySetup-1.0.10-msvc-x86.exe'
   file64        = Join-Path $toolsDir 'StrawberrySetup-1.0.10-msvc-x64.exe'
   softwareName  = 'Strawberry Music Player*'
   silentArgs    = "/S"
@@ -15,9 +16,10 @@ if($additionalArgs['InstallDir']) {
   $packageArgs['silentArgs'] += " /D=$($additionalArgs['InstallDir'])"
 }
 
+& "$toolsDir\chocolateyuninstall.ps1"
 Install-ChocolateyInstallPackage @packageArgs
 
 Remove-Item `
-  -Path $packageArgs['file64'] `
+  -Path $packageArgs['file'], $packageArgs['file64'] `
   -ErrorAction SilentlyContinue `
   -Force
