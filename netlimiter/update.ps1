3e4373a30bedc9d05b5bce3a63c5f54b66c07987
Jakub Levý <jakub.levy15@gmail.com>
2022-12-09 16:28:03 +0100

    netlimiter update fixed

--- netlimiter/update.ps1
+++ netlimiter/update.ps1
@@ -14,10 +14,10 @@ function global:au_SearchReplace {
 
 function global:au_GetLatest {
     $download_page = Invoke-WebRequest -UseBasicParsing -Uri 'https://www.netlimiter.com/download'
-    $relative_url = $download_page.links | ? href -match '/download/nl/' | Select-Object -exp href | Select-Object -first 1
+    $relative_url = $download_page.links | ? href -match '/nl/' | Select-Object -exp href | Select-Object -first 1
     $version = ([regex]::Match($relative_url, 'netlimiter-(\d+\.\d+(\.\d+)*)')).Groups[1].Value
     @{
-        Url32        = "https://www.netlimiter.com/download/nl/netlimiter-$version.exe"
+        Url32        = "https://download.netlimiter.com/nl/netlimiter-$version.exe"
         Version      = $version
         ReleaseNotes = "https://www.netlimiter.com/releases/$($version -replace '\.', '-')"
     }
