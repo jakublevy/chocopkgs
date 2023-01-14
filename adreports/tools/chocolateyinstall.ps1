b1f9f06fae5bfb9412a9ec9679435bb260681077
Chocolatey Packages <chocopkgs@gmail.com>
2023-01-14 16:06:31 +0100

    AU: 1 updated - adreports [skip ci]

--- adreports/tools/chocolateyinstall.ps1
+++ adreports/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@ $toolsDir = Split-Path -parent $MyInvocation.MyCommand.Definition
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
-  fileFullPath   = Join-Path $toolsDir 'adreports-1.4.7-win32.zip'
-  fileFullPath64 = Join-Path $toolsDir 'adreports-1.4.7-win64.zip'
+  fileFullPath   = Join-Path $toolsDir 'adreports-1.4.8-win32.zip'
+  fileFullPath64 = Join-Path $toolsDir 'adreports-1.4.8-win64.zip'
   destination    = "$toolsDir\bin"
   validExitCodes = @(0)
 }
