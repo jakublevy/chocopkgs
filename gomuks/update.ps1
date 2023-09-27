a4cf040ab4225046d2f1a62dca6888b2d911a1b0
Jakub Levý <jakub.levy15@gmail.com>
2023-09-27 12:47:56 +0200

    gomuks update fix

--- gomuks/update.ps1
+++ gomuks/update.ps1
@@ -17,7 +17,12 @@ function global:au_SearchReplace {
 function global:au_GetLatest {
     $rel = (Get-GitHubLatestReleaseLinks -user 'tulir' -repository 'gomuks').Links | % href
     $relative_url  = $rel | Where-Object { $_ -match '/tulir/gomuks/releases/download/v\d+\.\d+(\.\d+)*/gomuks-windows-amd64\.exe' } | Select-Object -First 1
+    if($null -eq $relative_url) {
+        $version = '0.3.0'
+    }
+    else {
     $version = ([regex]::Match($relative_url, 'v(\d+\.\d+(\.\d+)*)')).Groups[1].Value
+    }
     @{
         Version      = $version
         Url64        = "https://github.com/tulir/gomuks/releases/download/v$version/gomuks-windows-amd64.exe"
