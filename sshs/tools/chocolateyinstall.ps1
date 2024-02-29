e0234429a529f7fe7acac71cb685e6130a6f1966
Jakub Levý <jakub.levy15@gmail.com>
2024-03-01 00:32:04 +0100

    sshs remove 32-bit support

--- sshs/tools/chocolateyinstall.ps1
+++ sshs/tools/chocolateyinstall.ps1
@@ -1,17 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop'
 $toolsDir              = Split-Path -Parent $MyInvocation.MyCommand.Definition
-$removeFile            = "$toolsDir\sshs-windows-386.exe"
 $renameFile            = "$toolsDir\sshs-windows-amd64.exe"
-
-if((Get-OSArchitectureWidth -Compare 32) -or $env:ChocolateyForceX86) {
-  $removeFile = "$toolsDir\sshs-windows-amd64.exe"
-  $renameFile = "$toolsDir\sshs-windows-386.exe"
-}
-
-Remove-Item `
-  -Path "$toolsDir\sshs.exe", $removeFile `
-  -Force `
-  -ErrorAction SilentlyContinue
 
 Rename-Item `
   -Path $renameFile `
--- sshs/update.ps1
+++ sshs/update.ps1
@@ -5,9 +5,7 @@ import-module au
 function global:au_SearchReplace {
     @{
         ".\legal\VERIFICATION.txt" = @{
-            "(?i)(\s+x86:).*"          = "`${1} $($Latest.Url32)"
-            "(?i)(\s+x64:).*"          = "`${1} $($Latest.Url64)"
-            "(?i)(\s+checksum32:).*"   = "`${1} $($Latest.Checksum32)"
+            "(?i)(\s+Go to).*"          = "`${1} $($Latest.Url64)"
             "(?i)(\s+checksum64:).*"   = "`${1} $($Latest.Checksum64)"
         }
         ".\sshs.nuspec" = @{
@@ -21,7 +19,6 @@ function global:au_GetLatest {
     $relative_url  = $rel | Where-Object { $_ -match '/quantumsheep/sshs/releases/download/\d+\.\d+(\.\d+)*/sshs-windows-amd64\.exe' } | Select-Object -First 1
     $version = ([regex]::Match($relative_url, '/(\d+\.\d+(\.\d+)*)/')).Groups[1].Value
     @{
-        Url32        = "https://github.com/quantumsheep/sshs/releases/download/$version/sshs-windows-386.exe"
         Url64        = "https://github.com/quantumsheep/sshs/releases/download/$version/sshs-windows-amd64.exe"
         Version      = $version
         ReleaseNotes = "https://github.com/quantumsheep/sshs/releases/tag/v$version"
