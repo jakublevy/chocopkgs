ac05b0ebc12a9bed54161db5b792e68681a3953a
Chocolatey Packages <chocopkgs@gmail.com>
2024-03-08 00:08:41 +0100

    AU: 2 updated - ugrep z3 [skip ci]

--- z3/tools/chocolateyinstall.ps1
+++ z3/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@ $toolsDir = Split-Path -parent $MyInvocation.MyCommand.Definition
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
-  fileFullPath   = Join-Path $toolsDir 'z3-4.12.6-x86-win.zip'
-  fileFullPath64 = Join-Path $toolsDir 'z3-4.12.6-x64-win.zip'
+  fileFullPath   = Join-Path $toolsDir 'z3-4.13.0-x86-win.zip'
+  fileFullPath64 = Join-Path $toolsDir 'z3-4.13.0-x64-win.zip'
   destination    = $toolsDir
   validExitCodes = @(0)
 }
