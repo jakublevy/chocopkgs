0caedb7f9513234a2ff1a0225c2d1bd826575b6b
Chocolatey Packages <chocopkgs@gmail.com>
2024-09-06 00:07:24 +0200

    AU: 1 updated - ddh [skip ci]

--- ddh/tools/chocolateyinstall.ps1
+++ ddh/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@ $toolsDir = Split-Path -parent $MyInvocation.MyCommand.Definition
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
-  fileFullPath64 = Join-Path $toolsDir 'ddgst-3.0-x86_64-windows-msvc.zip'
+  fileFullPath64 = Join-Path $toolsDir 'ddgst-3.0.1-x86_64-windows-msvc.zip'
   destination    = "$toolsDir\bin"
   validExitCodes = @(0)
 }
