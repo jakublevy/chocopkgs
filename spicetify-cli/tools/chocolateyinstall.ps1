a05201322d5e6a0b8c7748218e81a09ac0f14fc6
Chocolatey Packages <chocopkgs@gmail.com>
2025-03-01 08:10:22 +0100

    AU: 1 updated - spicetify-cli [skip ci]

--- spicetify-cli/tools/chocolateyinstall.ps1
+++ spicetify-cli/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@ $toolsDir = Split-Path -parent $MyInvocation.MyCommand.Definition
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
-  fileFullPath   = Join-Path $toolsDir 'spicetify-2.39.4-windows-x32.zip'
-  fileFullPath64 = Join-Path $toolsDir 'spicetify-2.39.4-windows-x64.zip'
+  fileFullPath   = Join-Path $toolsDir 'spicetify-2.39.5-windows-x32.zip'
+  fileFullPath64 = Join-Path $toolsDir 'spicetify-2.39.5-windows-x64.zip'
   destination    = "$toolsDir\bin"
   validExitCodes = @(0)
 }
