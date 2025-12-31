5e95ecbd69ae64c9ffe7cc82da6b8c39fd7d9f49
Jakub Levý <6751890+jakublevy@users.noreply.github.com>
2025-12-31 11:56:50 +0100

    Get-RedirectedUrl added

--- .scripts/Get-GithubLatestReleaseLinks.ps1
+++ .scripts/Get-GithubLatestReleaseLinks.ps1
@@ -22,3 +22,26 @@ function Get-GitHubLatestReleaseLinks() {
     $assets_page = Invoke-WebRequest -Uri $expanded_assets_url -UseBasicParsing
     return $assets_page
   }
+
+# https://stackoverflow.com/questions/63042390/getting-the-final-redirected-url-from-within-powershell
+function Get-RedirectedUrl {
+    Param (
+        [Parameter(Mandatory=$true)]
+        [String]$url
+    )
+
+    $request = [System.Net.WebRequest]::Create($url)
+    $request.AllowAutoRedirect=$true
+    $request.UserAgent = 'Mozilla/5.0 (Windows NT; Windows NT 10.0; en-US) AppleWebKit/534.6 (KHTML, like Gecko) Chrome/7.0.500.0 Safari/534.6'
+    
+    try
+    {
+        $response = $request.GetResponse()
+        $response.ResponseUri.AbsoluteUri
+        $response.Close()
+    }
+    catch
+    {
+        "Error: $_"
+    }
+}
