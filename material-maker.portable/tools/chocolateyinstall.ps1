f1ceddd6db807db15ba0cba124c8460bbd458717
chocopkgs <chocopkgs@gmail.com>
2026-01-18 15:06:22 +0000

    AU: 2 updated - material-maker material-maker.portable [skip ci]

--- material-maker.portable/tools/chocolateyinstall.ps1
+++ material-maker.portable/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@ $toolsDir = Split-Path -parent $MyInvocation.MyCommand.Definition
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
-  fileFullPath64 = Join-Path $toolsDir 'material_maker_1_4_windows.zip'
+  fileFullPath64 = Join-Path $toolsDir 'material_maker_1_5_windows.zip'
   destination    = $toolsDir
   validExitCodes = @(0)
 }
