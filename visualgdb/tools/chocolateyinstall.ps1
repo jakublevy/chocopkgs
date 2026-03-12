11769f8057de0d1d255b43ca9cb588e662ce6dcc
chocopkgs <chocopkgs@gmail.com>
2026-03-12 07:05:43 +0000

    AU: 1 updated - visualgdb [skip ci]

--- visualgdb/tools/chocolateyinstall.ps1
+++ visualgdb/tools/chocolateyinstall.ps1
@@ -1,5 +1,5 @@
 ﻿$ErrorActionPreference = 'Stop'
-$absUrl   = 'http://eu.cdn.sysprogs.com/2593/VisualGDB-6.1r2-trial.msi'
+$absUrl   = 'http://eu.cdn.sysprogs.com/2610/VisualGDB-6.1r3-trial.msi'
 $checksum = ''
 
 $packageArgs = @{
