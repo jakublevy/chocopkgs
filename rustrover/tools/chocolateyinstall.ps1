b89f64bd15ad9ce58702cf73a2290fc88a81b4a5
chocopkgs <chocopkgs@gmail.com>
2026-03-30 14:05:18 +0000

    AU: 1 updated - rustrover [skip ci]

--- rustrover/tools/chocolateyinstall.ps1
+++ rustrover/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop'
 $toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
-$version  = '2025.3.5'
+$version  = '2026.1'
 $checksum64 = ''
 
 $silentConfig = "$toolsDir\silent.config"
