5aa3a0f5961a602d7d2dc0ba4ea1144b2f6f8442
chocopkgs <chocopkgs@gmail.com>
2026-04-21 06:05:02 +0000

    AU: 1 updated - clipstudio-paint [skip ci]

--- clipstudio-paint/tools/chocolateyinstall.ps1
+++ clipstudio-paint/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop'
 $toolsDir              = Split-Path -Parent $MyInvocation.MyCommand.Definition
-$version               = '502'
+$version               = '504'
 $checksum64            = ''
 $issFile               = "$toolsDir\install.iss"
 $languageFilesRoot     = "$env:ProgramData\CELSYS\CLIPStudio\InstallPath"
