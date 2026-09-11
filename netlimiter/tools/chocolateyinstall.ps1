47e17f383033e40b5c50db2de58b131da6ee0f70
chocopkgs <chocopkgs@gmail.com>
2026-09-11 22:05:31 +0000

    AU: 1 updated - netlimiter [skip ci]

--- netlimiter/tools/chocolateyinstall.ps1
+++ netlimiter/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop'
-$version  = '5.3.26.0'
-$checksum = '88ff734727889475440ccab119c007966210ad7dbf1a2ddf850589b91f64e7d5'
+$version  = '5.3.27.0'
+$checksum = ''
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
